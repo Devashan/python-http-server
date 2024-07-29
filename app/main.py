8d01cd1f99a42fc0a983033da85d7773d24077b4
Devashan Naicker <devii@clife.co.za>
2024-07-29 21:31:08 +0200

    Read header & user-agent

--- app/main.py
+++ app/main.py
@@ -23,6 +23,11 @@ def main():
                 string_length = len(echo_string)
                 str_type = 'text/plain'
                 response = b"HTTP/1.1 200 OK\r\nContent-Type: " + str_type.encode() + b"\r\nContent-Length: " + str(string_length).encode() + b"\r\n\r\n" + echo_string
+            elif (data.split(b"\r\n")[0].split(b" ")[1] == b"/user-agent"):
+                user_agent = data.split(b"\r\n")[1].split(b"User-Agent: ")[1].split(b"\r\n")[0]
+                user_agent_length = len(user_agent)
+                str_type = 'text/plain'
+                response = b"HTTP/1.1 200 OK\r\nContent-Type: " + str_type.encode() + b"\r\nContent-Length: " + str(user_agent_length).encode() + b"\r\n\r\n" + user_agent
             else:
                 response = b"HTTP/1.1 404 Not Found\r\n\r\n"
             print(response)
